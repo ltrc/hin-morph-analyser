4898b6a98e05205a2b5d4af675c0f985c0fb96f1
Nehal J Wani <nehaljw.kkd1@gmail.com>
2015-12-04 22:29:14 +0530

    Return type for get_paradigm() sould be void, comment unused variable

--- lib/morph-le/hin/analyser/get_paradigm.c
+++ lib/morph-le/hin/analyser/get_paradigm.c
@@ -14,12 +14,12 @@
 #include <string.h>
 #include "defn.h"
 
-get_paradigm(word,pdgm)
+void get_paradigm(word,pdgm)
   char word[WORD_SIZE];        /* morph word */
   char pdgm[WORD_SIZE];        /* paradigm */
   {
 	int n;
-	struct exp_pdgm *tag;  /* guessed paradigm object of strucure exp_pdgm */
+	//struct exp_pdgm *tag;  /* guessed paradigm object of strucure exp_pdgm */
 	pdgm[0]='\0';
 	n = strlen(word);
    /* takes the last letter of the word and checks the guessed paradigm from the
